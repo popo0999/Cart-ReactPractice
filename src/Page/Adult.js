f6f2a2d7491985341fab48bbf77b79849a52ce0d
LIZIHRONG <asbkk3697@gmail.com>
2021-10-09 01:02:25 +0800

    修正年齡完畢

--- src/Page/Adult.js
+++ src/Page/Adult.js
@@ -2,24 +2,24 @@ import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 
 import 'react-datepicker/dist/react-datepicker.css'
+import Todo from './Todo'
 
 function Adult() {
 	const [startDate, setStartDate] = useState(new Date())
 	const today = new Date()
-
-	const Example = () => {}
 	return (
 		<>
 			<DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 			<button
 				onClick={() => {
-					console.log('Example here')
-					const age = today - startDate
+					const age =
+						today / 1000 / 60 / 60 / 24 / 365 -
+						5 / 365 -
+						startDate / 1000 / 60 / 60 / 24 / 365
 					console.log('today', today)
 					console.log('startDate', startDate)
 					console.log(age)
-					console.log(age - 567907200000)
-					if (age > 567993624297) {
+					if (age > 18) {
 						alert('你已超過18')
 					} else {
 						alert('你還沒18耶...')
